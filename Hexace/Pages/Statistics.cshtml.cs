130bbff1d7d7f21d7dfd183b3aa1bc4eaf1a3e39
GooDCactuS <Marvele98@gmail.com>
2019-11-15 12:24:23 +0400

    Added database connection string

--- Hexace/Pages/Statistics.cshtml.cs
+++ Hexace/Pages/Statistics.cshtml.cs
@@ -1,16 +1,36 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Protocols;
 
 namespace Hexace.Pages
 {
     public class StatisticsModel : PageModel
     {
+        public List<int> Statistics { get; set; } = new List<int>();
+        
+
         public void OnGet()
         {
+            //string connectionString = ConfigurationManager<>.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //    SqlCommand command = new SqlCommand { Connection = connection };
+            //    command.CommandText = $"SELECT * ";
+            //    var reader = command.ExecuteReader();
+            //    if (reader.HasRows)
+            //    {
+            //        while (reader.Read())
+            //        {
+                        
+            //        }
+            //    }
+            //}
+
+            Statistics.Add(350);
+            Statistics.Add(220);
+            Statistics.Add(470);
         }
     }
 }
