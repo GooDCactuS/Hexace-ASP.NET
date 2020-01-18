7c3be4dc0af70ed16b8bff2f1efee35d1e62b0dd
GooDCactuS <Marvele98@gmail.com>
2020-01-18 22:00:50 +0400

    Added leaderboards logic and edited visualization

--- Hexace/Controllers/LeaderboardController.cs
+++ Hexace/Controllers/LeaderboardController.cs
@@ -2,15 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hexace.Data;
+using Hexace.Data.Objects;
+using Hexace.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hexace.Controllers
 {
     public class LeaderboardController : Controller
     {
+        private UserContext db;
+
+        public LeaderboardController(UserContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new LeaderboardModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LeaderboardModel model)
+        {
+            IQueryable<Profile> users = null;
+            
+            switch (model.ButtonValue)
+            {
+                case "ta": // Turquoise Attack
+                    users = db.Profiles.Where(x=>x.FractionId==1).OrderBy(x=>x.SuccessfulAttacks).Take(5);
+                    break;
+
+                case "td": // Turquoise Defence
+                    users = db.Profiles.Where(x => x.FractionId == 1).OrderBy(x => x.SuccessfulDefences).Take(5);
+                    break;
+
+                case "ba": // Burgundy Attack
+                    users = db.Profiles.Where(x => x.FractionId == 2).OrderBy(x => x.SuccessfulAttacks).Take(5);
+                    break;
+
+                case "bd": // Burgundy Defence
+                    users = db.Profiles.Where(x => x.FractionId == 2).OrderBy(x => x.SuccessfulDefences).Take(5);
+                    break;
+
+                case "pa": // Purple Attack
+                    users = db.Profiles.Where(x => x.FractionId == 3).OrderBy(x => x.SuccessfulAttacks).Take(5);
+                    break;
+
+                case "pd": // Purple Defence
+                    users = db.Profiles.Where(x => x.FractionId == 3).OrderBy(x => x.SuccessfulDefences).Take(5);
+                    break;
+            }
+            foreach (var item in users)
+            {
+                var userInfo = new LeaderboardModel.UserInfo();
+                userInfo.ProfileInfo = item;
+                userInfo.Nickname = db.Users.Where(x => x.Id == item.UserId).First().Nickname;
+                model.Users.Add(userInfo);
+            }
+            return View("Index", model);
         }
     }
 }
--- Hexace/Models/LeaderboardModel.cs
+++ Hexace/Models/LeaderboardModel.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Hexace.Data.Objects;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hexace.Models
 {
-    public class LeaderboardModel : PageModel
+    public class LeaderboardModel 
     {
-        public void OnGet()
+        public class UserInfo
         {
-
+            public Profile ProfileInfo { get; set; }
+            public string Nickname { get; set; }
         }
+        public List<UserInfo> Users { get; set; }
+
+        public string ButtonValue { get; set; }
+
+        public LeaderboardModel() { Users = new List<UserInfo>();}
+
+        public LeaderboardModel(List<UserInfo> users)
+        {
+            Users = users;
+        }
+
     }
 }
