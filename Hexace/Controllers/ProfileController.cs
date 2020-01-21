ff8f96b81d0e80b38071c3043f54405bcc8fb3d5
GooDCactuS <Marvele98@gmail.com>
2020-01-22 03:50:46 +0400

    Added password encryption

--- Hexace/Controllers/ProfileController.cs
+++ Hexace/Controllers/ProfileController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Hexace.Data;
 using Hexace.Data.Objects;
@@ -37,8 +39,8 @@ namespace Hexace.Controllers
         {
             if (ModelState["Email"].ValidationState == ModelValidationState.Valid && ModelState["Password"].ValidationState == ModelValidationState.Valid)
             {
-                var user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
-                if (user != null)
+                var user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user != null && VerifyHashedPassword(user.Password, model.Password))
                 {
                     await Authenticate(model.Email);
 
@@ -76,7 +78,7 @@ namespace Hexace.Controllers
                         {
                             Nickname = model.Nickname,
                             Email = model.Email,
-                            Password = model.Password,
+                            Password = HashPassword(model.Password),
                             RegistrationDate = DateTime.Today.Date,
                             UserType = "Player"
                         });
@@ -107,6 +109,25 @@ namespace Hexace.Controllers
             return View("Login", model);
         }
 
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+
         private async Task Authenticate(string email)
         {
             var claims = new List<Claim>
@@ -115,6 +136,42 @@ namespace Hexace.Controllers
             };
             ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null)
+            {
+                return false;
+            }
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0))
+            {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
+            {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+
+            for (int i = 0; i < buffer3.Length; i++)
+            {
+                if (buffer3[i] != buffer4[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         [Authorize]
